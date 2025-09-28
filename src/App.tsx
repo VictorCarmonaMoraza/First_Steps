592dcaaaab179abba740e87f5a84081a50613f97
Victor Carmona <victorcarmona9035.com>
2025-09-28 20:53:33 +0200

    Creacion de proyecto y modificacion de pagina principal

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hola Victor</h1>
+      <p>Hello Victor</p>
+    </>
+  );
+}
+
+export default App;

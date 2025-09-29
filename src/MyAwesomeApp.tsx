b9c77572760ad19b8bf5a0cd2d8440906ad4eb08
Victor Carmona <victorcarmona9035.com>
2025-09-29 11:02:29 +0200

    Impresion de variables

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,24 @@
+const name = "Pedro";
+const lastName = "Rodriguez";
+
+const favorite = ["Elden Ring", "Smash", "Metal Gear"];
+const isActive = true;
+
+const adress = {
+  zipCode: "ABC-123",
+
+  country: "Canada",
+};
 export function MyAwesomeApp() {
   return (
     <>
-      <h1>Victor</h1>
-      <h3>Carmona</h3>
+      <h1>{name}</h1>
+      <h3>{lastName}</h3>
+      {/* Unidos por una coma */}
+      <p>{favorite.join(", ").toUpperCase()}</p>
+
+      <h1>{isActive ? "Activo" : "Inactivo"}</h1>
+      <p>{JSON.stringify(adress)}</p>
     </>
   );
 }

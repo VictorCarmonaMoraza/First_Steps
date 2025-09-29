695f9558bc1369e79cc15e06db80f04e8668c849
Victor Carmona <victorcarmona9035.com>
2025-09-29 18:19:28 +0200

    Hook-useState

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,10 +1,22 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 import { ItemCounterProps } from "./ItemCounterProps";
 
-export const ItemCounter = ({ name, quantity }: ItemCounterProps) => {
-  const handleClick = () => {
-    console.log(`Click en ${name}`);
+export const ItemCounter = ({ name, quantity = 1 }: ItemCounterProps) => {
+  //Los hooks van al principio
+  const [count, setCount] = useState(quantity);
+
+  const hadleAdd = () => {
+    setCount(count + 1);
   };
+  const hadleSubtract = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  //   const handleClick = () => {
+  //     console.log(`Click en ${name}`);
+  //   };
   const estilo: CSSProperties = {
     display: "flex",
     alignItems: "center",
@@ -24,14 +36,16 @@ export const ItemCounter = ({ name, quantity }: ItemCounterProps) => {
         onMouseEnter={() => {
           console.log(`Mouse Enter ${name}`);
         }}
-        onClick={() => {
-          console.log(`Click ${name}`);
-        }}
+        // onClick={() => {
+        //   console.log(`Click ${name}`);
+        // }}
+        onClick={hadleAdd}
       >
         +1
       </button>
-      <span>{quantity}</span>
-      <button onClick={handleClick}>-1</button>
+      <span>{count}</span>
+      {/* <button onClick={handleClick} onClick={hadleSubtract}> */}
+      <button onClick={hadleSubtract}>-1</button>
     </section>
   );
 };
